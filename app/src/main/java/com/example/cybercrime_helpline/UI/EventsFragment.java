abb9e0a6d4f6cbaba7f240310cbc4f50f2f3dca5
PRAJWOL <prajwolbogati9@gmail.com>
2020-01-18 08:50:06 +0545

    Events Fragment

--- app/src/main/java/com/example/cybercrime_helpline/UI/EventsFragment.java
+++ app/src/main/java/com/example/cybercrime_helpline/UI/EventsFragment.java
@@ -2,14 +2,18 @@ package com.example.cybercrime_helpline.UI;
 
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.cybercrime_helpline.R;
 
 public class EventsFragment extends Fragment {
+    Button btnorganizeevents,btnshowevents;
+
 
 
     public EventsFragment() {
@@ -21,7 +25,26 @@ public class EventsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events, container, false);
-    }
+        View root = inflater.inflate(R.layout.fragment_events, container, false);
+        btnorganizeevents = root.findViewById(R.id.btnorganizeevents);
+        btnshowevents = root.findViewById(R.id.btnshowevents);
 
+        btnorganizeevents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), OrganizeEventActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnshowevents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ViewEventActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return root;
+    }
 }
