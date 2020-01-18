0e4b9419a7641433a736165bf05b36b7c1d689d0
PRAJWOL <prajwolbogati9@gmail.com>
2020-01-18 14:16:59 +0545

    Strict Mode

---
+++ app/src/main/java/com/example/cybercrime_helpline/StrictMod/StrictMode.java
@@ -0,0 +1,11 @@
+package com.example.cybercrime_helpline.StrictMod;
+
+public class StrictMode {
+
+    public static void StrictMode() {
+        android.os.StrictMode.ThreadPolicy policy = new android.os.StrictMode.ThreadPolicy.Builder().permitAll().build();
+        android.os.StrictMode.setThreadPolicy(policy);
+    }
+}
+
+
