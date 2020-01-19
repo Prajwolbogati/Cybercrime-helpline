90a21a9ab2629182dea4974b4dca2e1fc2e8327d
PRAJWOL <prajwolbogati9@gmail.com>
2020-01-19 22:58:54 +0545

    Url connection and responseimage model

---
+++ app/src/main/java/com/example/cybercrime_helpline/Models/ResponseImage.java
@@ -0,0 +1,13 @@
+package com.example.cybercrime_helpline.Models;
+
+public class ResponseImage {
+    private String filename;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+}
