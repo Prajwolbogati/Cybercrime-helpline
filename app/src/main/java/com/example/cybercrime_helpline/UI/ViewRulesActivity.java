ddb74db85492d2f61bae25c613b4c91ad8ea4c62
PRAJWOL <prajwolbogati9@gmail.com>
2020-01-27 10:04:58 +0545

    View Law Activity

--- app/src/main/java/com/example/cybercrime_helpline/UI/ViewRulesActivity.java
+++ app/src/main/java/com/example/cybercrime_helpline/UI/ViewRulesActivity.java
@@ -1,17 +1,119 @@
 package com.example.cybercrime_helpline.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Toast;
 
+import com.example.cybercrime_helpline.Adapter.AdapterLaw;
+import com.example.cybercrime_helpline.Interfaces.InterfaceLaw;
+import com.example.cybercrime_helpline.Models.Rules;
 import com.example.cybercrime_helpline.R;
 
-public class ViewRulesActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ViewRulesActivity extends AppCompatActivity {
+    private RecyclerView recycleView;
+    List<Rules> rulesList= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_rules);
+        recycleView = findViewById(R.id.rulesrecycleview);
+
+        recycleView.setLayoutManager(new GridLayoutManager(this,2));
+        recycleView.addItemDecoration(new ViewRulesActivity.GridSpacingItemDecoration(2, dpToPx(3), true));
+        recycleView.setItemAnimator(new DefaultItemAnimator());
+        recycleView.setAdapter(new AdapterLaw(getApplicationContext(),rulesList));
+
+        readRules();
+    }
+
+    private void readRules() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:3000/rules/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        InterfaceLaw Apirules = retrofit.create(InterfaceLaw.class);
+
+        Call<List<Rules>> listCall = Apirules.getalllaws();
+
+        listCall.enqueue(new Callback<List<Rules>>() {
+            @Override
+            public void onResponse(Call<List<Rules>> call, Response<List<Rules>> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Code: " + response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                List<Rules> rulesModelList = response.body();
+
+                recycleView.setAdapter(new AdapterLaw(getApplicationContext(), rulesModelList));
+            }
+
+            @Override
+            public void onFailure(Call<List<Rules>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "Error " + t.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 }
