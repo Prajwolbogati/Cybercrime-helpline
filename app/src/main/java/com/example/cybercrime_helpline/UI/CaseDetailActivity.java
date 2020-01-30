472b1f2488023df2b0c60a31afd7331c49762fdf
PRAJWOL <prajwolbogati9@gmail.com>
2020-01-30 19:43:51 +0545

    Case Detail Activity

---
+++ app/src/main/java/com/example/cybercrime_helpline/UI/CaseDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.cybercrime_helpline.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cybercrime_helpline.R;
+
+public class CaseDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_case_detail);
+    }
+}
