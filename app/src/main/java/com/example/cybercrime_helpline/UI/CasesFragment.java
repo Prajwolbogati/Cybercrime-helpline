9ac41d155433f548959aee6f3b03e268a76368e6
PRAJWOL <prajwolbogati9@gmail.com>
2020-01-31 19:20:02 +0545

    Case Fragment

--- app/src/main/java/com/example/cybercrime_helpline/UI/CasesFragment.java
+++ app/src/main/java/com/example/cybercrime_helpline/UI/CasesFragment.java
@@ -2,14 +2,17 @@ package com.example.cybercrime_helpline.UI;
 
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.cybercrime_helpline.R;
 
 class CasesFragment extends Fragment {
+    Button btnaddcase,btnshowcases;
 
 
     public CasesFragment() {
@@ -21,7 +24,26 @@ class CasesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cases, container, false);
-    }
+        View root = inflater.inflate(R.layout.fragment_cases, container, false);
+        btnaddcase = root.findViewById(R.id.btnaddcase);
+        btnshowcases = root.findViewById(R.id.btnshowcases);
 
+        btnaddcase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), FileCaseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnshowcases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ViewCaseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return root;
+    }
 }
