283d8a12bf1d5e577429247451c8e7e8802c9cdf
PRAJWOL <prajwolbogati9@gmail.com>
2020-02-05 18:43:50 +0545

    Login Activity

--- app/src/main/java/com/example/cybercrime_helpline/UI/LoginActivity.java
+++ app/src/main/java/com/example/cybercrime_helpline/UI/LoginActivity.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.cybercrime_helpline.BLL.Loginbll;
 import com.example.cybercrime_helpline.MainActivity;
 import com.example.cybercrime_helpline.R;
+import com.example.cybercrime_helpline.StrictMod.StrictMode;
+import com.example.cybercrime_helpline.url.Url;
 
 import java.util.Objects;
 
@@ -20,7 +24,10 @@ public class LoginActivity extends AppCompatActivity {
     private EditText username;
     private EditText logpassword;
     private Button btnlog;
-    private ImageView logimage;
+    private Button btnreg;
+    private boolean isSuccess = false;
+
+    public String BASE_URL = "http://10.0.2.2:3000/";
 
 
     @Override
@@ -30,36 +37,42 @@ public class LoginActivity extends AppCompatActivity {
         username = findViewById(R.id.username);
         logpassword = findViewById(R.id.logpassword);
         btnlog = findViewById(R.id.btnlog);
-        logimage = findViewById(R.id.logimage);
+        btnreg = findViewById(R.id.btnreg);
+
+        btnreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
 
         btnlog.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                if(Objects.equals(username.getText().toString(), "admin")&& Objects.equals(logpassword.getText().toString(),"admin"))
-                {
-                    Toast.makeText(LoginActivity.this,"You have Authenticated Successfully",Toast.LENGTH_LONG).show();{
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+                final Loginbll bll = new Loginbll(username.getText().toString(), logpassword.getText().toString());
+                StrictMode.StrictMode();
+
+                if (bll.checkUser()) {
+                    Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+                    intent.putExtra("userid", Url.userid);
                     startActivity(intent);
-                }
-                }else
-                {
-                    Toast.makeText(LoginActivity.this,"Authentication Failed",Toast.LENGTH_LONG).show();
+                    finish();
                 }
             }
         });
-        Login();
-            }
-
-
-    private void Login(){
-        SharedPreferences sharedPreferences = getSharedPreferences("User",MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        editor.putString("username",username.getText().toString());
-        editor.putString("password",logpassword.getText().toString());
-        editor.commit();
-
-        Toast.makeText(this,"Successfully Login", Toast.LENGTH_SHORT).show();
     }
+    public boolean nullValidation(){
+        if (TextUtils.isEmpty(username.getText().toString())){
+            username.setError("Required Field");
+            return false;
+        }
+        else if (TextUtils.isEmpty(logpassword.getText().toString())){
+            logpassword.setError("Required Field");
+            return false;
+        }
+        return true;
+    }
+
 }
