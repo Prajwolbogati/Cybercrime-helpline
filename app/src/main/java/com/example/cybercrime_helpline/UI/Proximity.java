ccbef84f68b74d8ffc67ec1f2b2c5f0982471375
PRAJWOL <prajwolbogati9@gmail.com>
2020-02-07 08:28:48 +0545

    Sensor added

---
+++ app/src/main/java/com/example/cybercrime_helpline/UI/Proximity.java
@@ -0,0 +1,50 @@
+package com.example.cybercrime_helpline.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.cybercrime_helpline.R;
+
+public class Proximity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proximity);
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        final Sensor proximitysensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        SensorEventListener sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+
+                if (sensorEvent.values[0] < proximitysensor.getMaximumRange()) {
+
+
+                    Toast.makeText(Proximity.this,"Application Closed",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Proximity.this, MotionSensor.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(Proximity.this,"Far",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+        sensorManager.registerListener(sensorEventListener,proximitysensor,2*1000*1000);
+
+    }
+}
+
